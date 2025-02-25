22703391e7f004dc85cfe2d0d0fc28078bae71c0
vittorio cadoni <v.cadoni@dstech.it>
2025-02-25 13:48:49 +0100

    FirstCommit

---
+++ infrastruttura/src/main/java/it/testfinale1/infrastruttura/InfrastrutturaApplication.java
@@ -0,0 +1,13 @@
+package it.testfinale1.infrastruttura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrastrutturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfrastrutturaApplication.class, args);
+	}
+
+}
---
+++ infrastruttura/src/test/java/it/testfinale1/infrastruttura/InfrastrutturaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.testfinale1.infrastruttura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastrutturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
